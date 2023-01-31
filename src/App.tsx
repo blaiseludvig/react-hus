45953016ad2da8e464a14a4d08f34c0838da4836
blaiseludvig <blaiseludvig@gmail.com>
2023-01-31 15:22:50 +0100

    implement CRUD features

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import ScrewCard from "./ScrewCard";
+import "reflect-metadata";
+import { plainToInstance } from "class-transformer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ScrewData {
+  id!: number;
+  type!: string;
+  size!: number;
+  stock!: number;
+  price!: number;
 }
+
+const App = () => {
+  const [screws, setScrews] = useState(new Set<ScrewData>());
+  const [newType, setNewType] = useState("");
+  const [newSize, setNewSize] = useState("");
+  const [newStock, setNewStock] = useState("");
+  const [newPrice, setNewPrice] = useState("");
+
+  const loadData = async () => {
+    const response = await fetch("http://localhost:3000/api/screw");
+    const data: Object[] = await response.json();
+
+    setScrews(new Set(plainToInstance(ScrewData, data)));
+  };
+
+  const addScrew = async (screwData: Omit<ScrewData, "id">) => {
+    const response = await fetch(`http://localhost:3000/api/screw`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(screwData),
+    });
+
+    const data: Object = await response.json();
+
+    setScrews(new Set(screws).add(plainToInstance(ScrewData, data)));
+  };
+
+  const deleteScrew = async (id: number) => {
+    const target = [...screws].find((target) => target.id === id);
+
+    const newSet = new Set(screws);
+    newSet.delete(target!);
+    setScrews(newSet);
+
+    fetch(`http://localhost:3000/api/screw/${id}`, { method: "DELETE" });
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <>
+      <div className="container my-4" style={{ maxWidth: "1024px" }}>
+        <div className="row justify-content-start mx-auto mb-4">
+          <div className="form-floating mb-3 col-sm-3">
+            <input
+              className="form-control"
+              value={newType}
+              onChange={(event) => {
+                setNewType(event.target.value);
+              }}
+            ></input>
+            <label className="mx-2 mb-1">Típus:</label>
+          </div>
+
+          <div className="form-floating mb-3 col-sm-3">
+            <input
+              className="form-control"
+              value={newSize}
+              onChange={(event) => {
+                setNewSize(event.target.value);
+              }}
+            ></input>
+            <label className="mx-2 mb-1">Méret (mm):</label>
+          </div>
+
+          <div className="form-floating mb-3 col-sm-3">
+            <input
+              className="form-control"
+              value={newStock}
+              onChange={(event) => {
+                setNewStock(event.target.value);
+              }}
+            ></input>
+            <label className="mx-2 mb-1">Készlet:</label>
+          </div>
+
+          <div className="form-floating mb-3 col-sm-3">
+            <input
+              className="form-control"
+              value={newPrice}
+              onChange={(event) => {
+                setNewPrice(event.target.value);
+              }}
+            ></input>
+            <label className="mx-2 mb-1">Ár:</label>
+          </div>
+
+          <button
+            className="btn btn-outline-secondary btn-lg btn-block"
+            onClick={() => {
+              if (newType && newSize && newStock && newPrice) {
+                addScrew({
+                  type: newType,
+                  size: parseInt(newSize),
+                  stock: parseInt(newStock),
+                  price: parseFloat(newPrice),
+                });
+              }
+            }}
+          >
+            Hozzáad
+          </button>
+        </div>
+
+        <div className="row gap-3 justify-content-start mx-auto">
+          {[...screws].map((screw) => (
+            <ScrewCard
+              key={crypto.randomUUID()}
+              id={screw.id}
+              type={screw.type}
+              stock={screw.stock}
+              size={screw.size}
+              price={screw.price}
+              deleteCallback={() => deleteScrew(screw.id)}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
